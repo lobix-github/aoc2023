3f5dbf9601e63708e7074f21681ca1ba3ee17362
Łukasz Łobacz <lulo@demant.com>
2023-12-04 11:21:15 +0100

    Improved day 4

--- aoc/d04.cs
+++ aoc/d04.cs
@@ -3,31 +3,17 @@
     public void Run()
     {
         var sum = 0;
-        var lines = File.ReadLines(@"..\..\..\inputs\04.txt").Select(x => x.Split(':')[1]);
-        var list = new List<List<(HashSet<int>, HashSet<int>)>>();
-        foreach (var card in lines)
+        var lines = File.ReadLines(@"..\..\..\inputs\04.txt").Select(x => x.Split(':')[1]).ToArray();
+        var cards = new List<int>(Enumerable.Repeat(1, lines.Count()));
+        for (int i = 0; i < lines.Count(); i++)
         {
-            var wins = new HashSet<int>(card.Split('|')[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ToInt));
-            var nums = new HashSet<int>(card.Split('|')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ToInt));
-            sum += (int)Math.Pow(2, wins.Intersect(nums).Count() - 1);
-
-            list.Add(new List<(HashSet<int>, HashSet<int>)>() { (wins, nums) });
+            var wins = new HashSet<int>(lines[i].Split('|')[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ToInt));
+            var nums = new HashSet<int>(lines[i].Split('|')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ToInt));
+            var count = wins.Intersect(nums).Count();
+            sum += (int)Math.Pow(2, count - 1);
+            Enumerable.Range(1, count).ToList().ForEach(x => cards[i + x] += cards[i]);
         }
         Console.WriteLine(sum); // part 1
-
-        for (int i = 0; i < list.Count; i++)
-        {
-            var cards = list[i];
-            foreach (var (wins, nums) in cards)
-            {
-                var count = wins.Intersect(nums).Count();
-                for (int j = 1; j <= count; j++)
-                {
-                    list[i + j].Add(list[i + j][0]);
-                }
-            }
-        }
-        sum = list.Select(x => x.Count()).Sum();
-        Console.WriteLine(sum); // part 2
+        Console.WriteLine(cards.Sum()); // part 2
     }
 }
