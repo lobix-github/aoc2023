294dd844b50f9093681c0d921c3d6fc38ad90147
Łukasz Łobacz <lulo@demant.com>
2023-12-04 10:39:29 +0100

    Day 4

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -3,3 +3,4 @@ new d01_2().Run();
 new d02().Run();
 new d03_1().Run();
 new d03_2().Run();
+new d04().Run();
---
+++ aoc/d04.cs
@@ -0,0 +1,33 @@
+﻿class d04 : baseD
+{
+    public void Run()
+    {
+        var sum = 0;
+        var lines = File.ReadLines(@"..\..\..\inputs\04.txt").Select(x => x.Split(':')[1]);
+        var list = new List<List<(HashSet<int>, HashSet<int>)>>();
+        foreach (var card in lines)
+        {
+            var wins = new HashSet<int>(card.Split('|')[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ToInt));
+            var nums = new HashSet<int>(card.Split('|')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ToInt));
+            sum += (int)Math.Pow(2, wins.Intersect(nums).Count() - 1);
+
+            list.Add(new List<(HashSet<int>, HashSet<int>)>() { (wins, nums) });
+        }
+        Console.WriteLine(sum); // part 1
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            var cards = list[i];
+            foreach (var (wins, nums) in cards)
+            {
+                var count = wins.Intersect(nums).Count();
+                for (int j = 1; j <= count; j++)
+                {
+                    list[i + j].Add(list[i + j][0]);
+                }
+            }
+        }
+        sum = list.Select(x => x.Count()).Sum();
+        Console.WriteLine(sum); // part 2
+    }
+}
