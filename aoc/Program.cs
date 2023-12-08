7de653fef3902fe37d81eb14d24f5516af64e0a8
Łukasz Łobacz <lulo@demant.com>
2023-12-08 12:39:51 +0100

    Day 8

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -7,3 +7,4 @@ new d04().Run();
 new d05().Run();
 new d06().Run();
 new d07().Run();
+new d08().Run();
---
+++ aoc/d08.cs
@@ -0,0 +1,41 @@
+﻿class d08 : baseD
+{
+    public void Run()
+    {
+        var lines = File.ReadLines(@"..\..\..\inputs\08.txt")
+            .Select(x => x.Replace("=", ""))
+            .Select(x => x.Replace("(", ""))
+            .Select(x => x.Replace(",", ""))
+            .Select(x => x.Replace(")", ""))
+            .ToArray();
+
+        var nodes = lines[2..]
+            .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            .ToDictionary(x => x[0], x => (x[1], x[2]));
+
+        long count = trace(nodes["AAA"], x => x == "ZZZ");
+        Console.WriteLine(count); // part 1
+
+        var keysA = nodes.Keys.Where(k => k.EndsWith('A'));
+        var paths = new List<long>();
+        foreach (var keyA in keysA)
+        {
+            count = trace(nodes[keyA], x => x.EndsWith('Z'));
+            paths.Add(count);
+        }
+        count = Numerics.Lcm(paths);
+        Console.WriteLine(count); // part 2
+
+        long trace((string, string) node, Func<string, bool> stop)
+        {
+            long count = 0;
+            while (true)
+            {
+                var next = lines[0][(int)count++ % lines[0].Length] == 'L' ? node.Item1 : node.Item2;
+                if (stop(next)) break;
+                node = nodes[next];
+            }
+            return count;
+        }
+    }
+}
