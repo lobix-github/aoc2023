d3c0a0bef2ed17fcb447e4ec845b28e22cf1ba45
lulo <lulo@demant.com>
2023-12-15 09:09:44 +0100

    Day 15 Part 1

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -12,4 +12,5 @@
 //new d10().Run();
 //new d11().Run();
 //new d13().Run();
-new d14().Run();
+//new d14().Run();
+new d15().Run();
---
+++ aoc/d15.cs
@@ -0,0 +1,12 @@
+﻿class d15 : baseD
+{
+    public void Run()
+    {
+        var lines = string.Join(Environment.NewLine, File.ReadLines(@"..\..\..\inputs\15.txt")).Split(',');
+
+        var sum = lines.Select(getHash).Sum();
+        Console.WriteLine(sum); // part 1
+
+        int getHash(string val) => val.Aggregate(0, (hash, c) => hash = ((hash + c) * 17) % 256);
+    }
+}
